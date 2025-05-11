6d73f258d31c81f4f6604c014fcc687830537262
Zevi Liao <jointjm@gmail.com>
2025-05-11 15:17:54 +0800

    keep previous value

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import RefSetFocusInput from "./components/RefSetFocusInput";
-// import RefPreValue from "./components/RefPreValue";
+import RefPreValue from "./components/RefPreValue";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,13 +13,12 @@ function App() {
   return (
     <>
       <h2>ref demo</h2>
-      <RefSetFocusInput />
-      {/* <RefPreValue/> */}
-      <div>
+      <RefPreValue/>
+      {/* <div>
         <button 
           onClick={handleChangeColor}
-        style={{ padding: "20 30" }}>change bg color to red</button>
-      </div>
+        style={{ padding: "20 30" }}>number count</button>
+      </div> */}
     </>
   );
 }
--- src/components/RefPreValue.jsx
+++ src/components/RefPreValue.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from "react";
 
 const RefPreValue = () => {
-  return (
-    <div>
-      RefPreValue
-    </div>
-  )
-}
+  const prevVal = useRef(0);
+  const [count, setCount] = useState(0);
+  // console.log(prevVal)
+  const handleAddClick = () => {
+    setCount((prev) => {
+      prevVal.current = prev;
+      return prev + 3;
+    });
+    // console.log("test", prevVal.current);
+  };
 
-export default RefPreValue
+  // useEffect(() => {
+  //   console.log("test2", prevVal.current);
+  // }, [prevVal.current]);
+
+  return (
+    <div style={{ width: 400 }}>
+      <h3>RefPreValue</h3>
+      <section>
+        <p> current value: {count}</p>
+        <p> previous value: {prevVal.current}</p>
+        <button onClick={handleAddClick}>update value</button>
+      </section>
+    </div>
+  );
+};
+
+export default RefPreValue;
