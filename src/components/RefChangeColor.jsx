f4e0b9087e61371a1eb8f089ba1999cd6ff36932
Zevi Liao <jointjm@gmail.com>
2025-05-11 14:09:55 +0800

    forwardRef

--- src/components/RefChangeColor.jsx
+++ src/components/RefChangeColor.jsx
@@ -1,22 +1,18 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-const RefChangeColor = (props) => {
-  const { ref } = props;
-
+const RefChangeColor = forwardRef((props, ref) => {
   console.log("test", props);
   return (
-    <>
-      <div
-        ref={ref}
-        style={{
-          backgroundColor: "yellow",
-          padding: 30,
-        }}
-      >
-        RefChangeColor
-      </div>
-    </>
+    <div
+      ref={ref}
+      style={{
+        backgroundColor: "yellow",
+        padding: 30,
+      }}
+    >
+      RefChangeColor
+    </div>
   );
-};
+});
 
 export default RefChangeColor;
