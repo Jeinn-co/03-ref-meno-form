68811deb60a840b77d9e9955aec4834696bbec80
Zevi Liao <jointjm@gmail.com>
2025-05-11 21:04:29 +0800

    value hello

--- src/components/ComponentA.jsx
+++ src/components/ComponentA.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import ComponentB from "./ComponentB";
+
+export const MessageContext = createContext(null);
 
 export default function ComponentA() {
   const [message, setMessage] = useState("Initial message from ComponentA");
 
   return (
-    <div style={{ backgroundColor: 'yellow'}}>
-      <h2>ComponentA (1 Level)</h2>
-      <button onClick={() => setMessage("Updated at " + new Date().toLocaleTimeString())}>
-        Update Message
-      </button>
-      <ComponentB text={message} />
-    </div>
+    <MessageContext.Provider value="hello">
+      <div style={{ backgroundColor: "yellow" }}>
+        <h2>ComponentA (1 Level)</h2>
+        <button
+          onClick={() =>
+            setMessage("Updated at " + new Date().toLocaleTimeString())
+          }
+        >
+          Update Message
+        </button>
+        <ComponentB text={message} />
+      </div>
+    </MessageContext.Provider>
   );
 }
--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MessageContext } from "./ComponentA";
 
 export default function ComponentC({ text }) {
+  const value = useContext(MessageContext);
+  console.log(value); // "hello"
+
   return (
-    <div style={{ paddingLeft: 40, backgroundColor: 'lightgreen'}}>
+    <div style={{ paddingLeft: 40, backgroundColor: "lightgreen" }}>
       <h4>ComponentC (3 Level)</h4>
       <p>Received message: {text}</p>
+      <p>{value}</p>
+      {/* <button onClick={() => value.setMessage("update!!")}>update</button> */}
     </div>
   );
 }
