0469eff164a39a4d996d7a6228fe76eab6260dde
Zevi Liao <jointjm@gmail.com>
2025-05-11 14:39:57 +0800

    Set focus

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import { useState, useRef, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import RefChangeColor from "./components/RefChangeColor";
-// import RefSetFocusInput from "./components/RefSetFocusInput";
+import RefSetFocusInput from "./components/RefSetFocusInput";
 // import RefPreValue from "./components/RefPreValue";
 
 function App() {
   const [count, setCount] = useState(0);
-  const refDiv = useRef();
-
-  // console.log('test', refDiv)
-
-  // useEffect(() => {
-  //   if (refDiv.current) {
-  //     console.log("effect test", refDiv);
-  //     // refDiv.current.style.backgroundColor = 'red'
-  //   }
-  // }, []);
 
   const handleChangeColor = () =>{
-    // refDiv.current.style.backgroundColor = 'red'
-    refDiv.current.innerText = 'Updated'
   }
 
   return (
     <>
       <h2>ref demo</h2>
-      <RefChangeColor ref={refDiv} />
-      {/* <RefSetFocusInput /> */}
+      <RefSetFocusInput />
       {/* <RefPreValue/> */}
       <div>
         <button 
